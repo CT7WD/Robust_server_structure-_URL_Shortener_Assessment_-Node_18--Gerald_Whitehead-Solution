6808f674ae0b001849636c328045a577e0712b92
CT7WD <63139550+CT7WD@users.noreply.github.com>
2024-05-16 08:28:52 -0400

    Added READ

--- src/urls/urls.controller.js
+++ src/urls/urls.controller.js
@@ -5,31 +5,54 @@ function list(req, res) {
     res.json({ data: urlsData.filter(id ? url => url.id == id : () => true) });
 }
 
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data: {} } = req.body;
+    if (data[propertyName]) {
+      return next()
+    }
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`
+    })
+  }
+}
+
 let lastUrlId = urlsData.reduce((maxId, url) => Math.max(maxId, url.id), 0);
 
-// function bodyDataHas(propertyName) {
-//     return function (req, res, next) {
-//       const { data = {} } = req.body;
-//       if (data[propertyName]) {
-//         return next();
-//       }
-//       next({
-//           status: 400,
-//           message: `Must include a ${propertyName}`
-//       });
-//     };
-//   }
 
-  function create(req, res) {
-    const { data: { id, href } = {} } = req.body;
-    const newUrl = {
-      id: ++lastUrlId, // Increment last id then assign as the current ID
-      href: href,
-    };
-    urlsData.push(newUrl);
-    res.status(201).json({ data: newUrl });
+//FIND IF URL EXISTS
+function urlExists(req, res, next) {
+  const { urlId } = req.params;
+  const foundUrl = urlsData.find(url => url.id === Number(urlId));
+  if (foundUrl) {
+    res.locals.urlsData = foundUrl;
+    return next();
   }
+  next({
+    status: 404,
+    message: `URL id not found: ${urlId}`,
+  });
+};
 
+
+
+// ***CREATE***
+function create(req, res) {
+  const { data: { href, id } = {} } = = req.body;
+  const newUrl = {
+    href,
+    id: ++lastUrlId
+  }
+  urlsData.push(newUrl)
+  res.status(201).json({ data: newUrl })
+}
+
+
+// ***READ***
+  function read(req, res) {
+    
+  }
 // Export the create function to be used in routes
 module.exports = {
     create: [
